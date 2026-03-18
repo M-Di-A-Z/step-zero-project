d7bfc9b2db6b243a46459fa27f35c4d617bed9af
alexis hebrard <alexishebrard2001@gmail.com>
2026-03-18 09:45:22 +0100

    fix bug and button

--- db/migrate/20260317154717_drop_favorites.rb
+++ db/migrate/20260317154717_drop_favorites.rb
@@ -1,9 +1,5 @@
 class DropFavorites < ActiveRecord::Migration[8.1]
   def change
-    drop_table :favorites do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :business_idea, null: false, foreign_key: true
-      t.timestamps
-    end
+    drop_table :favorites, if_exists: true
   end
 end
