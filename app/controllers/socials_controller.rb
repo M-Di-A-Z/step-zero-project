d6cafd0f8d1bbec12f9cd02c9e17d56845f1ac9d
alexis hebrard <alexishebrard2001@gmail.com>
2026-03-18 12:44:40 +0100

    trending

--- app/controllers/socials_controller.rb
+++ app/controllers/socials_controller.rb
@@ -9,10 +9,20 @@ class SocialsController < ApplicationController
       .limit(2)
       .pluck(:business_idea_id)
 
-    @trending_posts = trending_ids.any? ? BusinessIdea.where(id: trending_ids).includes(:user, :likes, comments: :user) : []
+    @trending_posts = trending_ids.any? ? BusinessIdea.where(id: trending_ids).includes(:user, :likes, comments: :user).to_a : []
+
+    if @trending_posts.size < 2
+      exclude_ids = trending_ids + @trending_posts.map(&:id)
+      filler = BusinessIdea.where(shared: true)
+                           .where.not(id: exclude_ids)
+                           .includes(:user, :likes, comments: :user)
+                           .order(created_at: :desc)
+                           .limit(2 - @trending_posts.size)
+      @trending_posts += filler.to_a
+    end
 
     @posts = BusinessIdea.where(shared: true)
-                         .where.not(id: trending_ids)
+                         .where.not(id: @trending_posts.map(&:id))
                          .includes(:user, :likes, comments: :user)
                          .order(created_at: :desc)
   end
